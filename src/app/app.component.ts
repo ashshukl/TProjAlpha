f276faafaf73bf6a8fd44a28547621b8f1f9229f
ashshukl <ashishshukla321@hotmail.com>
2023-08-04 17:24:32 +0530

    Contains Angular Code
    Contains Angular Code

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  constructor(private router: Router){
+
+  }
+
+  title = 'The Project Omega';
+  
+  isDisabled: boolean = false;
+  
+  dynamicTxt:string = 'This is dynamic text';
+
+  btnClick(){
+    console.log('I was clicked');
+  }
+
+  TClicked(){
+      this.router.navigate(['abc']);
+  }
+
+  SClicked(){
+    this.router.navigate(['xyz']);
+  }
+}
